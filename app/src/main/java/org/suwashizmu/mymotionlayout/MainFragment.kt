3bfc5d25c45bf12c9ba5945aab565ce400f94b2c
suwashimizu <suwawa2013@gmail.com>
2019-03-23 17:03:03 +0900

    used transition

--- app/src/main/java/org/suwashizmu/mymotionlayout/MainFragment.kt
+++ app/src/main/java/org/suwashizmu/mymotionlayout/MainFragment.kt
@@ -1,6 +1,7 @@
 package org.suwashizmu.mymotionlayout
 
 import android.os.Bundle
+import android.transition.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -27,18 +28,30 @@ class MainFragment : Fragment(), MotionLayout.TransitionListener {
 
         motionLayout.setTransitionListener(this)
         image.setOnClickListener {
-            motionLayout.transitionToStart()
+            //            motionLayout.transitionToStart()
+            moveToSecond()
         }
     }
 
     private fun moveToSecond() {
 
-        if (requireFragmentManager().findFragmentById(R.id.details) == null)
+        if (requireFragmentManager().findFragmentById(R.id.details) == null) {
+
+            val secondFragment = SecondFragment.newInstance().apply {
+                sharedElementEnterTransition = DetailsTransition()
+                sharedElementReturnTransition = DetailsTransition()
+
+            }
+            exitTransition = Fade()
+
             requireFragmentManager()
                 .beginTransaction()
-                .add(R.id.details, SecondFragment.newInstance())
+                .addSharedElement(image, "image")
+                .replace(R.id.container, secondFragment)
                 .addToBackStack(null)
                 .commit()
+        }
+
     }
 
 
@@ -55,6 +68,15 @@ class MainFragment : Fragment(), MotionLayout.TransitionListener {
     override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
         if (p1 == R.id.end) {
             moveToSecond()
+        }
+    }
+
+    private class DetailsTransition : TransitionSet() {
+        init {
+            ordering = ORDERING_TOGETHER
+            addTransition(ChangeBounds())
+                .addTransition(ChangeTransform())
+                .addTransition(ChangeImageTransform())
         }
     }
 }
