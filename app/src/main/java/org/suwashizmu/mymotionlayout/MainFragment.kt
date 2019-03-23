6f0814c8138547062350bfff84ea5011350444ab
suwashimizu <suwawa2013@gmail.com>
2019-03-23 15:41:26 +0900

    motion for main

--- app/src/main/java/org/suwashizmu/mymotionlayout/MainFragment.kt
+++ app/src/main/java/org/suwashizmu/mymotionlayout/MainFragment.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.main_frag.*
 
 /**
  * Created by KEKE on 2019/03/20.
  */
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), MotionLayout.TransitionListener {
 
     companion object {
         fun newInstance() =
@@ -24,8 +25,9 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        motionLayout.setTransitionListener(this)
         image.setOnClickListener {
-            moveToSecond()
+            motionLayout.transitionToStart()
         }
     }
 
@@ -37,5 +39,22 @@ class MainFragment : Fragment() {
                 .add(R.id.details, SecondFragment.newInstance())
                 .addToBackStack(null)
                 .commit()
+    }
+
+
+    override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+
+    }
+
+    override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+    }
+
+    override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+    }
+
+    override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+        if (p1 == R.id.end) {
+            moveToSecond()
+        }
     }
 }
